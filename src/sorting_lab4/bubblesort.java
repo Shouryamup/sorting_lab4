0152da379c4f966987bcff83cdecf2c9075ef50e
Shouryamup <mshourya9@gmail.com>
2021-02-11 23:30:36 +0530

    bubble sort

---
+++ src/sorting_lab4/bubblesort.java
@@ -0,0 +1,37 @@
+
+package sorting_lab4;
+import java.util.Scanner;
+
+public class bubblesort {
+	 public static void main(String []args) {
+		    int n, c, d, swap;
+		    Scanner in = new Scanner(System.in);
+		 
+		    System.out.println("Input number of integers:");
+		    n = in.nextInt();
+		 
+		    int array[] = new int[n];
+		 
+		    System.out.println("Enter " + n + " integers:");
+		 
+		    for (c = 0; c < n; c++)
+		      array[c] = in.nextInt();
+		   
+		    for (c = 0; c < ( n - 1 ); c++) {
+		      for (d = 0; d < n - c - 1; d++) {
+		        if (array[d] > array[d+1]) 
+		        {
+		          swap       = array[d];
+		          array[d]   = array[d+1];
+		          array[d+1] = swap;
+		        }
+		      }
+		    }
+		 
+		    System.out.println("Sorted list:");
+		 
+		    for (c = 0; c < n; c++)
+		      System.out.print(array[c]+" ");
+		  }
+	
+}
